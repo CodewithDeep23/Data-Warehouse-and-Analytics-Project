7675dc25062986550c4e81f2bc93636bf6112b50
CodewithDeep23 <deepankarsinghvm@gmail.com>
2025-06-15 10:31:10 +0530

    Add DROP TABLE statements for existing Bronze tables in DDL script

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,6 +8,7 @@ Script Purpose:
 ===============================================================================
 */
 
+DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
     cst_id              INT,
     cst_key             VARCHAR(50),
@@ -18,6 +19,7 @@ CREATE TABLE bronze.crm_cust_info (
     cst_create_date     DATE
 );
 
+DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
     prd_id       INT,
     prd_key      VARCHAR(50),
@@ -28,6 +30,7 @@ CREATE TABLE bronze.crm_prd_info (
     prd_end_dt   TIMESTAMP
 );
 
+DROP TABLE IF EXISTS bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num  VARCHAR(50),
     sls_prd_key  VARCHAR(50),
@@ -40,17 +43,20 @@ CREATE TABLE bronze.crm_sales_details (
     sls_price    INT
 );
 
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
     cid    VARCHAR(50),
     cntry  VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
     cid    VARCHAR(50),
     bdate  DATE,
     gen    VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     id           VARCHAR(50),
     cat          VARCHAR(50),
